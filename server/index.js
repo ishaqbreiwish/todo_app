65d0f8c44b2c21ee67e68535c7fbb0de4116f0ab
Ishaq Breiwish <ishaq.breiwish@gmail.com>
2025-01-31 18:28:58 -0500

    Fixed mongoDB implementation

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 app.use(cors())
 app.use(bodyParser.json())
 
-
+mongoose
     .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
